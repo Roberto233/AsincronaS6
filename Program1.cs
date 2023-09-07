bcc67882f9c8cd69139a13013506afe6758b2556
Roberto233 <141881391+Roberto233@users.noreply.github.com>
2023-09-06 19:49:30 -0600

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,18 @@
+﻿class whereSample
+{
+    static void Main()
+    {
+        int[] scores = {97, 92, 81, 60 };
+
+        var queryLowNums =
+            from score in scores
+            where score  > 80
+            select score;
+
+        foreach ( var s in queryLowNums )
+        { 
+            Console.WriteLine(s.ToString() + " ");
+        }
+
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,21 @@
+﻿class whereSample
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93, 75, 82};
+
+        var queryLowNums =
+            from score in scores
+            where score > 80
+            orderby score descending
+            select score;
+
+
+      
+        foreach (var s in queryLowNums)
+        {
+            Console.WriteLine(s.ToString() + " ");
+        }
+
+    }
+}
